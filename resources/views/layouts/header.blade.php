59077ec5eda85887373187a11672711ca5b09c08
chikafu7 <taylantuzcuoglu0@gmail.com>
2026-05-09 21:39:33 +0300

    düzeltme

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,9 +7,13 @@
 
             <div class="flex flex-wrap items-center gap-2 p-2 rounded-xl border" style="background-color:rgba(255,241,242,.15);border-color:rgba(255,241,242,.30);">
                 <a href="{{ route('articles.index') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffdfe5;">Makaleler</a>
-                <a href="{{ route('journals.create') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffdfe5;">+ Yeni Dergi</a>
-                <a href="{{ route('issues.create') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffe7eb;">+ Yeni Sayı</a>
 
+                @auth
+                    @if(auth()->user()->isAdmin() || auth()->user()->isEditor())
+                        <a href="{{ route('journals.create') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffdfe5;">+ Yeni Dergi</a>
+                        <a href="{{ route('issues.create') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffe7eb;">+ Yeni Sayı</a>
+                    @endif
+                @endauth
                 @guest
                     <a href="{{ route('login') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffe7eb;">Giriş Yap</a>
                     <a href="{{ route('register') }}" class="font-semibold px-3 py-2 rounded-lg" style="color:#5f0f1f;background-color:#ffe7eb;">Kayıt Ol</a>
